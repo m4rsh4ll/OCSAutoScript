63f7379131c96c8d1b97ad0eb820141682b7bb7b
m4rsh4ll <38029582+m4rsh4ll@users.noreply.github.com>
2018-04-07 05:00:19 +0800

    Create trial.sh

---
+++ trial.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Script auto create trial user SSH
+#will expired after 1 day
+#modified by Clrkz
+
+IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+
+Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
+hari="1"
+Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
+
+useradd -e `date -d "$hari days" +"%Y-%m-%d"` -s /bin/false -M $Login
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e ""
